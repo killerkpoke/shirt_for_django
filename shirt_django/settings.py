e7819852415aac2f70b6e814ec08b5226919d99e
David Kohalmi <killerkpoke@gmail.com>
2021-04-18 20:26:27 +0200

    models added

--- shirt_django/settings.py
+++ shirt_django/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+    'product',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -127,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
